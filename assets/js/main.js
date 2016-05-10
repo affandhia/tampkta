6e7ba76e7c2205525074d7195c3a05bfdca83db8
Affan <affandhia@gmail.com>
2016-05-11 01:02:51 +0700

    added header and been parallaxed

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,18 @@
 var controller = new ScrollMagic.Controller();
 
 // create a scene
-new ScrollMagic.Scene({
-        duration: $(".pahlawan").eq("0").offset().top - $("#header").offset().top,    // the scene should last for a scroll distance of 100px
-        offset: 0        // start this scene after scrolling for 50px
+var scene1 = new ScrollMagic.Scene({
+        duration: $(".pahlawan").eq("0").offset().top - $("#scrolldown").offset().top,    // the scene should last for a scroll distance of 100px
+        offset: $("#scrolldown").offset().top  // start this scene after scrolling for 50px
     })
     .setPin("#scrolldown") // pins the element for the the scene's duration
     .addTo(controller); // assign the scene to the controller
+
+var hController = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+
+var header = new ScrollMagic.Scene({triggerElement: "#parallax1"})
+	.setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
+	.addTo(hController);
 
 (function($) {
     var wh = window.innerHeight,
